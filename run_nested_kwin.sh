22436c1cc830fe3db5fcdffb1c5c6fff04f8fbad
Johan Grande <nahoj+git@crans.org>
2025-05-03 17:48:53 +0200

    .

--- run_nested_kwin.sh
+++ run_nested_kwin.sh
@@ -2,34 +2,33 @@
 # run-nested-kde.sh
 
 # 1) Prepare isolated runtime
-export XDG_RUNTIME_DIR=$HOME/.cache/many/runtime
-rm -rf   "$XDG_RUNTIME_DIR"
-mkdir -p "$XDG_RUNTIME_DIR"
-chmod 700 "$XDG_RUNTIME_DIR"
+# export XDG_RUNTIME_DIR=$HOME/.cache/many/runtime
+# rm -rf   "$XDG_RUNTIME_DIR"
+# mkdir -p "$XDG_RUNTIME_DIR"
+# chmod 700 "$XDG_RUNTIME_DIR"
 
 # 2) Point your configs
 export KDEHOME=$HOME/.config/many/nestedconfig
-export XDG_CONFIG_HOME=$KDEHOME
-export XDG_DATA_HOME=$HOME/.local/share/many
-export XDG_CACHE_HOME=$HOME/.cache/many
+# export XDG_CONFIG_HOME=$KDEHOME
+# export XDG_DATA_HOME=$HOME/.local/share/many
+# export XDG_CACHE_HOME=$HOME/.cache/many
 
 # 3) Choose a single socket name
-host_wayland_display=$WAYLAND_DISPLAY
-export WAYLAND_DISPLAY=wayland-1
+sock=wayland-1
 
 # 4) Launch everything inside its own DBus session
-dbus-run-session -- \
-  kwin_wayland \
-    --windowed \
-    --socket $WAYLAND_DISPLAY \
-    --wayland-display $host_wayland_display &
+kwin_wayland \
+  --socket $sock \
+  --wayland-display $WAYLAND_DISPLAY &
 
 KWIN_PID=$!
 sleep 0.5
 
 # 5) Now launch Konsole into that nested compositor
-export QT_QPA_PLATFORM=wayland
+#export QT_QPA_PLATFORM=wayland
+export WAYLAND_DISPLAY=$sock
 konsole
 
-# 6) Cleanup when Konsole exits
-kill $KWIN_PID
+# 6) Wait for kwin
+wait $KWIN_PID
+
